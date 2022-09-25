d585deb5bcdd93a24ee52f66ad9cac82141fb053
Abdullajon1997 <abdullajonibrohimov0@gmail.com>
2022-09-25 19:41:13 +0300

    3-oy 4-vazifa

---
+++ main.js
@@ -0,0 +1,359 @@
+// 1-misol
+
+// let n = prompt ("Son kiriting");
+// n=parseInt(n);
+// function son ()
+// {
+//     let Array=[];
+//     for (let i=0;i<=n;i++)
+//     {
+//         Array.push(i);
+//     }
+//     document.write(Array);
+// }son()
+
+// 2-misol
+
+// let n = prompt ("Son kiriting");
+// n=parseInt(n);
+// function son ()
+// {
+//     let Array=[];
+//     for (let i=1;i<=n;i++)
+//     {
+//         Array.push(2**i);
+//     }
+//     document.write(Array);
+// }son()
+
+// 3-misol
+
+// let n = prompt ("Son kiriting");
+// // n=parseInt(n); 
+// function son ()
+// {
+//     let Array=[],S=0;
+//     for (let i=0;i<=n;i++)
+//     {
+//         Array.push(i);
+//         S+=i;
+//     }
+//     document.write(S);
+// }son()
+
+// 4-misol
+
+// function son ()
+// {
+//     let yigindi=0,ayirma=0;
+//     // Arr=parseInt(Arr);
+//     let Arr=[0,10,20,30,40,50,60,70,80,90,100];
+//     for (let i=0;i<Arr.length;i++)
+//     {
+//         if(i%2==1)
+//         { 
+//             console.log("dasd");
+//             console.log(Arr[i]);
+//             yigindi=yigindi+Arr[i];
+//             // console.log(yigindi);
+//         }
+//         else
+//         {
+//             ayirma-=Arr[i];
+//         }
+//     }
+//     return [yigindi,ayirma];
+// }console.log(son())
+
+// 5-misol
+
+
+// function son ()
+// { 
+//     let n= prompt("n ni kiriting:")
+//     let array=new Array(n);
+//     let a=0;
+//     let S=0;
+//     for (let i=0;i<n;i++)
+//     {
+//         a =Number( prompt ( `${i} - son kiriting`));
+//         array.push(a);
+//         S=S+a
+//         // n=parseInt(n);
+//     }
+//     document.write(S);
+    
+// }
+// son()
+
+// 6-misol
+
+// function son ()
+// {
+//     n=Number(prompt("Sonni kiriting:"))
+//     let arr=[];
+//     for (let i=n;i>=0;i--)
+//     {
+//         arr.push(i)
+//     }
+//     document.write(arr);
+// }son()
+
+// 8-misol
+
+// function son()
+// {
+//     k=Number(prompt("K sonni kiriting:"))
+//     l=Number(prompt("L sonni kiriting:"))
+//     let s=0;
+//     let arr=[2,5,12,36,63,54,57,67,98,75,25];
+//     for(let i=k;i<=l;i++)
+//     {
+//         s+=arr[i];
+//     }
+//     document.write(s);
+// }son()
+
+// 9-misol
+
+// function max ()
+// {
+//     let max=0;
+//     let arr=[2,5,12,36,63,54,57,607,98,75,125];
+//     for(let i=0;i<=arr.length;i++)
+//     {
+//         if(arr[i]>max)
+//         {
+//             max=arr[i]
+//         }
+//     }
+//     document.write(max);
+// }max ()
+
+// 10-misol
+
+// function min()
+// {
+//     let min;
+//     let arr=[2,1,12,36,63,54,-57,67,98,75,25];
+//     for( let i=0; i<=arr.length; i++)
+//     {
+//         if(arr[i]<=arr[0])
+//         {
+//             min=arr[i];
+//         }
+//     }
+//     document.write(min);
+// }min();
+
+// 11-misol
+
+// function max ()
+// {
+    
+//     let max=0;
+//     let arr=[2,5,786,12,785,36,63,54,57,607,98,75,625];
+//     for(let i=0;i<=arr.length;i++)
+//     {
+//         if(arr[i]%2==1)
+//         {
+//             if(arr[i]>max)
+//         {
+//             max=arr[i]
+//         }
+//         }
+//     }
+//     document.write(max);
+// }max ()
+
+// 12-misol
+
+// function son ()
+// {
+//     let s=0;
+//     const arr=[2,8,12,28,50];
+//     for(let i=0; i<arr.length; i++)
+//     {
+//         s+=arr[i];
+//     }
+//     s/=arr.length;
+//     document.write(s);
+// } son ()
+
+// 13-misol
+
+// function son ()
+// {
+//     const arr=[2,8,12,28,50,84,92,55,75,52];
+//     for(let i=0; i<arr.length; i++)
+//     {
+//         if(i%2==1)
+//         {
+//             document.write(arr[i]," , ")
+//         }
+//     }
+// } son ()
+
+// 14-misol
+
+// function son ()
+// {
+//     const arr=[2,8,12,28,50,14,25,37,86,95,132,658,778,146,58];
+//     for(let i=4; i<arr.length; i++)
+//     {
+//         if(i%2==0)
+//         {
+//             document.write(i," , ")
+//         }
+//     }
+//     document.write();
+// } son ()
+
+// 15-misol
+
+// function son ()
+// {
+//     const arr=[2,8,15,12,28,50,84,92,55,75,52];
+//     for(let i=0; i<=arr[i]; i++)
+//     {
+//         if(arr[i]%2==1)
+//         {
+//             document.write(arr[i]," , ")
+//         }
+//     }
+// } son ()
+
+// 16-misol
+
+// function son ()
+// {
+//     const arr=[2,"8",15,"12",28,50,"84",92,55,"75",256];
+//     for(let i=0; i<=arr[i]; i++)
+//     {
+//         if(typeof(arr[i])==="string")
+//         {
+//             document.write(arr[i]," , ")
+//         }
+//     }
+// } son ()
+
+// 17-misol
+
+// function son ()
+// {
+//     let kopaytma=1;
+//     const arr=[2,8,5,45,6,9,4,7];
+//     for(let i=0; i<=arr[i]; i++)
+//     {
+//         kopaytma*=arr[i];
+//     }
+//     document.write(kopaytma)
+// } son ()
+
+// 18-misol
+
+// function son ()
+// { 
+//     let n= prompt("n ni kiriting:")
+//     let array=new Array(n);
+//     let a=0;
+//     for (let i=0;i<n;i++)
+//     {
+//         a =Number( prompt ( `${i} - son kiriting`));
+//         array.push(a);
+//     }
+//     document.write(array);
+    
+// }
+// son()
+
+// 19-misol
+
+// function son ()
+// { 
+//     let n= prompt("n ni kiriting:")
+//     let array=new Array(n);
+//     let a=0;
+//     let S=0;
+//     for (let i=0;i<n;i++)
+//     {
+//         a =Number( prompt ( `${i} - son kiriting`));
+//         array.push(a);
+//         S=S+a
+//         // n=parseInt(n);
+//     }
+//     document.write(S);
+    
+// }
+// son()
+
+// 20-misol
+
+// function son ()
+// { 
+//     let n= prompt("n ni kiriting:")
+//     let arr=[];
+//     let a=0;
+//     let s='';
+//     for (let i=0;i<n;i++)
+//     {
+//             a =Number( prompt ( `${i} - son kiriting`));
+//             arr.push(a);
+//     }
+//     s=prompt(s);
+//     if(s=="juft")
+//     {
+//         for(let i=0;i<arr.length;i++)
+//         {
+//             if(i%2==0)
+//             {
+//                 document.write(arr[i],",");
+//             }
+//         }
+//     }
+//     else
+//     if(s=="toq")
+//     {
+//         for(let i=0;i<arr.length;i++)
+//         {
+//             if(i%2==1)
+//             {
+//                 document.write(arr[i],",");
+//             }
+//         }
+//     }
+// }
+// son()
+
+// 21-misol
+
+// function son ()
+// { 
+//     let n= prompt("n ni kiriting:")
+//     let array=[];
+//     let a=0;
+//     for (let i=0;i<n;i++)
+//     {
+//         a = Number( prompt ( `${i} - son kiriting`));
+//         array.push(a);
+//     }
+//     document.write(array[n-1]);
+// }
+// son()
+
+// 22-misol
+
+// function son ()
+// {
+//     let arr=[2,5,12,36,63,54,57,75,98,75,25,2];
+//     for(let i=0;i<=arr.length;i++)
+//     {
+//         for(let k=i+1;k<arr.length;k++)
+//         {
+//             if(arr[i]===arr[k])
+//             {
+//                 console.log(" i = " + arr[i], i, " k = " + arr[k], k);
+//             }
+//         }
+//     }
+// }son()
